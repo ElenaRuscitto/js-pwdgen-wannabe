a48fe7e33b6e80bc00dc8f8c214247090fa475f8
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-02-19 15:15:11 +0100

    [add] Milestone 2

--- js/script.js
+++ js/script.js
@@ -5,13 +5,14 @@ const cognomeUtente = prompt ('Quale è il tuo cognome?')
 const colorePreferitoUtente = prompt ('Quale è il tuo colore preferito?')
 console.log(nomeUtente, cognomeUtente, colorePreferitoUtente)
 
-const nomeUtenteCompleto = nomeUtente + ' ' + cognomeUtente
+const nomeUtenteCompleto = nomeUtente + cognomeUtente
 console.log('nome Utente Completo: ' , nomeUtenteCompleto)
 
-document.getElementById('nomCogCol').innerHTML = '
-  <p>   
-   Ciao ${nomeUtenteCompleto}, il tuo colore preferito è il ${colorePreferitoUtente} 24
-  </p>
-'
+document.getElementById('nomCogCol').innerHTML = `
+<p>   
+${nomeUtenteCompleto}${colorePreferitoUtente}24
+</p>
+`
+
 
 
